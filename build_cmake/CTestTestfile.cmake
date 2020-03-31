038ddd108607a55f87513eb1054c2327fd4eba0f
“marrascout” <929458522@qq.com>
2020-03-31 20:46:09 +0800

    cmake1

---
+++ build_cmake/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/marrascout/libsnark/libsnark
+# Build directory: /home/marrascout/libsnark/libsnark/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("depends")
+subdirs("libsnark")
