722bb05faf128f6c906dc473738fdebc13b4119c
“marrascout” <929458522@qq.com>
2020-03-31 21:23:06 +0800

    check

---
+++ build_check/depends/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/marrascout/libsnark/libsnark/depends/gtest/googlemock
+# Build directory: /home/marrascout/libsnark/libsnark/build/depends/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
